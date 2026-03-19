a773be4989902d71a135ce9e2740da3f9a8be911
Dhinesh Karthick <dhineshkarthick778@gmail.com>
2026-03-19 22:01:53 +0530

    Time: 47 ms (66.74%), Space: 29.2 MB (29.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,38 +2,46 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
 
-        
-        int sum = 0;
-        vector<int> single;
-        unordered_map<int,int> mp;
-        set<vector<int>> temp;
 
-        int target = 0;
+        
+        vector<int> single;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
 
         for(int i = 0; i < nums.size(); i ++)
-        {
-            target = target - nums[i];
-            
-            for(int j = i+1;j<nums.size();j++)
+        { 
+            if(i>0 && nums[i-1]==nums[i])
             {
-                sum = target - nums[j];
-
-                if(mp.find(sum)!=mp.end())
+                continue;
+            }
+            int left = i+1;
+            int right = nums.size()-1;
+            while(left<right)
+            {
+                int sum = 0;
+                sum = nums[i]+nums[left]+nums[right];
+                if(sum == 0)
                 {
                     single.push_back(nums[i]);
-                    single.push_back(nums[mp[sum]]);
-                    single.push_back(nums[j]);
-                    sort(single.begin(),single.end());
-                    temp.insert(single);
+                    single.push_back(nums[left]);
+                    single.push_back(nums[right]);
+                    ans.push_back(single);
                     single.clear();
+                    while(left < right && nums[left]==nums[left+1])left++;
+                    while(left<right && nums[right]==nums[right-1])right--;
+                    left++;
+                    right--;
                 }
-                mp[nums[j]]=j;
+                else if(sum>0)
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
             }
-            single.clear();
-            mp.clear();
-            target = 0;
         }
-        vector<vector<int>> ans(temp.begin(),temp.end());
         return ans;
     }
 };
